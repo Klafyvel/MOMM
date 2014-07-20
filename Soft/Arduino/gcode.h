e9dd7097922edc1589fdccb7413a33f48f12796f
Klafyvel <klafyvel@gmail.com>
2014-07-20 17:42:08 +0200

    Began to implement gcode

--- Soft/Arduino/gcode.h
+++ Soft/Arduino/gcode.h
@@ -1,15 +1,29 @@
 #ifndef H_GCODE
 #define H_GCODE
-/*
-// movings modes
-#define A
 
-// other G-code 
+//M-codes
+#define M02 0 // end prgm
+#define M03 1 // clockwise directions
+#define M05 2 // stop the spindle rotation
 
-class G_interpreter
+//G-codes
+#define G00 3 // linear movement, fast
+#define G01 4 // linear movement, milling
+#define G04 5 // delay
+#define G21 6 // metric mode
+#define G82 7 // drill
+#define G90 8 // absolute coordinate
+
+typedef void (*callback_parser)(Machine machine);
+
+callback_parser instructions[];
+
+struct parser_state
 {
 
+	int moving_mode;
+	// to be continue 
 };
-*/
+
 
 #endif
